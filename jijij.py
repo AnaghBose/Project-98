607978308937c71fdf3d63fc1a476d8335adc356
AnaghBose <112865566+AnaghBose@users.noreply.github.com>
2022-09-05 17:59:41 +0530

    Add files via upload

--- jijij.py
+++ jijij.py
@@ -5,12 +5,12 @@ chances = 0
 print("Guess a number(From 1 to 9):")
 while chances < 5:
     guess = int(input("Enter Your Guess:"))
-if guess == number:
-    print("Congratulation ! Your guess was correct !!")
-elif guess < number:
-    print("OOPS ! Your guess was lower than than the number: Guess a number higher than", guess)
-else:
-     print("Your guess was higher than the number: Guess a number higher than", guess)
+    if guess == number:
+        print("Congratulation ! Your guess was correct !!")
+    elif guess < number:
+        print("OOPS ! Your guess was lower than than the number: Guess a number higher than", guess)
+    else:
+        print("Your guess was higher than the number: Guess a number higher than", guess)
 chances += 1
 if not chances < 5:
     print("You lost ! The number was", number)
